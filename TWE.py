d20d49cc9644fac2fd149ff238cc16f1b909ddcb
thiemo seys <thiemoseys123@gmail.com>
2019-09-22 18:58:32 +0200

    added logging
    Added logging for easier debugging

--- TWE.py
+++ TWE.py
@@ -1,43 +1,53 @@
 import requests
-import urllib.request
-import time
 from bs4 import BeautifulSoup
 import datetime
 from managers import email
 import logging
 
+logging.basicConfig(level=logging.DEBUG, filename="app.log", filemode="w")
+
+
 #variables
 base = "https://www.thewhiskyexchange.com/new-products/"
-url_standard = base + "standard-whisky#nav"
-url_rare = base + "old-and-rare-whisky#nav"
 currentDay = datetime.datetime.now().day
 
 #global lsits
-urls = []
+urls = [base + "standard-whisky#nav", base + "old-and-rare-whisky#nav"]
 responses = []
 parsed_htmls = []
 previous_release_days = []
-#add data to lists
-urls.append(url_standard)
-urls.append(url_rare)
 
+
+logging.info("starting http requests")
 for url in urls:
+    logging.debug("get requesting:"+url)
     responses.append(requests.get(url))
 
+logging.info("starting html parsing")
 for response in responses:
     if response.status_code == 200:
         parsed_htmls.append(BeautifulSoup(response.text, "html.parser"))
+        logging.debug("parsed html")
+    else: logging.warning("failed to succesfuly request info, error code: "+response.status_code)
 
 
 for text in parsed_htmls:
-    list_days_strings = ([item.get_text(strip=True) for item in text.select("span.np-posthead__date-day")])
-    #parse days to int (there should be a cleaner way to do this right?
-    days = [int(i) for i in list_days_strings]
-    previous_release_days.append(days)
+    try:
+        list_days_strings = ([item.get_text(strip=True) for item in text.select("span.np-posthead__date-day")])
+        #parse days to int (there should be a cleaner way to do this right?
+        days = [int(i) for i in list_days_strings]
+        #appends all release days to a list, (technically we only need the latest release date) This is WIP, might make the code cleaner but less flexible if we only save the latest release date.
+        previous_release_days.append(days)
+    except ValueError as e: logging.critical("fault in parsing days: "+e)
 
 
 for releases in previous_release_days:
+    #checks if last release day equals the current day.
     if releases[0] == currentDay:
         print("new bottles have come out")
+        logging.info("new bottles have been released, sending email")
         email.send_email()
-    else: print("no new bottles have been release")
+    else:
+        print("no new bottles have been release")
+        logging.info("no new bottles have been released today!")
+
